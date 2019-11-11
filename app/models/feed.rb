ebbd52714843e0acafccf81aa434d4cc70346ecd
gortron <gordon.lanza@gmail.com>
2019-11-11 16:41:48 +0000

    formatting in feed.rb

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -2,43 +2,32 @@ class Feed < ActiveRecord::Base
   belongs_to :folder
   has_many :posts
 
+  
   def self.create_from_url(args)
     url = args["link"]
     folder_id = args["folder_id"].to_i
+    
     open(url) do |rss|
-    feed = RSS::Parser.parse(rss)
-    new_feed = Feed.create({
-      title: feed.channel.title,
-      link: feed.channel.link,
-      description: feed.channel.description,
-      #img_url: feed.image.url,
-      folder_id: folder_id
-    })
-    byebug
-    feed.items.each do |post|
-      Post.create({
-        title: post.title,
-        link: post.link,
-        description: post.description,
-        author_name: post.dc_creator,
-        published_time: post.pubDate,
-        feed_id: new_feed.id
+      feed = RSS::Parser.parse(rss)
+      new_feed = Feed.create({
+        title: feed.channel.title,
+        link: feed.channel.link,
+        description: feed.channel.description,
+        #img_url: feed.image.url,
+        folder_id: folder_id
       })
+
+      feed.items.each do |post|
+        Post.create({
+          title: post.title,
+          link: post.link,
+          description: post.description,
+          author_name: post.dc_creator,
+          published_time: post.pubDate,
+          feed_id: new_feed.id
+        })
+
     end
-    #self.create_from_feed(feed, new_feed.id)
     end
   end
-
-  # def self.create_from_feed(feed)
-  #   feed.items.each do |post|
-  #     Post.create({
-  #       title: post.title,
-  #       link: post.link,
-  #       description: post.description,
-  #       author_name: post.dc_creator,
-  #       published_time: post.pubDate,
-  #       feed_id: new_feed.id
-  #     })
-  #   end
-  # end
 end
