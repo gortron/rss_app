9242c1267afaa9722a2403ba0758c87450b63224
gortron <gordon.lanza@gmail.com>
2019-11-14 11:57:53 +0000

    re-ordered user validations to match order of sign up form

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,14 +5,16 @@ class User < ActiveRecord::Base
   has_many :bookmarks
 
   # Validations
-  has_secure_password
+
   validates :name, length: { minimum: 5 }
   validates :name, format: { with: /\s/, message: "should be your first and last names"}
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :email, uniqueness: true
   validates :username, presence: true
   validates :username, uniqueness: true
   validates :username, format: { without: /\s/, message: "Username must contain no spaces" }
-  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
-  validates :email, uniqueness: true
+  has_secure_password
+
   
 
   #flash[:errors] = person.errors.full_messages.to_sentence
